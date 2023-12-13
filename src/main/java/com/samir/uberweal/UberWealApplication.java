bbf65ff8dfbddde13574a67a6f8deec9b01f1045
Samir1498 <samirsbx2@gmail.com>
2023-12-13 10:59:28 +0100

    Init project

---
+++ src/main/java/com/samir/uberweal/UberWealApplication.java
@@ -0,0 +1,13 @@
+package com.samir.uberweal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberWealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberWealApplication.class, args);
+    }
+
+}
