492ebc690f3a8751a7aa765efa304b53b3fb20a3
Samir1498 <samirsbx2@gmail.com>
2023-12-20 07:29:24 +0100

    refactoring BookRide tests setup into a single class

---
+++ src/test/java/com/samir/uberweal/BookRideTestSetup.java
@@ -0,0 +1,57 @@
+package com.samir.uberweal;
+
+import com.samir.uberweal.application.commands.BookRideCommand;
+import com.samir.uberweal.core.domain.entities.Driver;
+import com.samir.uberweal.core.domain.entities.Location;
+import com.samir.uberweal.core.domain.entities.Rider;
+import com.samir.uberweal.core.domain.entities.ride.RideType;
+import com.samir.uberweal.core.domain.repositories.RiderRepository;
+import com.samir.uberweal.core.domain.repositories.RideRepository;
+import com.samir.uberweal.core.domain.repositories.stubs.RiderRepositoryStub;
+import com.samir.uberweal.core.domain.repositories.stubs.RideRepositoryStub;
+import com.samir.uberweal.core.domain.services.pricing.RideChargeCalculatorFactory;
+import com.samir.uberweal.core.domain.services.pricing.strategies.JourneyRideChargeCalculator;
+import com.samir.uberweal.core.domain.services.pricing.strategies.TripRideChargeCalculator;
+import com.samir.uberweal.application.commandhandlers.BookRideCommandHandler;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class BookRideTestSetup {
+
+    public final static RideRepository rideRepository = new RideRepositoryStub();
+
+    public static Rider setupRider() {
+        return Rider.builder()
+                .funds(100)
+                .id(1L)
+                .joinedAt(LocalDate.now())
+                .build();
+    }
+
+
+    public static BookRideCommandHandler setupBookRideCommandHandler() {
+        TripRideChargeCalculator tripRideChargeCalculator = new TripRideChargeCalculator();
+        JourneyRideChargeCalculator journeyRideChargeCalculator = new JourneyRideChargeCalculator();
+        RideChargeCalculatorFactory rideChargeCalculatorFactory = new RideChargeCalculatorFactory(tripRideChargeCalculator, journeyRideChargeCalculator);
+
+        return new BookRideCommandHandler(rideRepository, rideChargeCalculatorFactory);
+    }
+
+    public static BookRideCommand buildBookRideCommand(
+            Rider rider,
+            RideType type,
+            Location startPoint,
+            Location destination,
+            double distance
+    ) {
+        return BookRideCommand.builder()
+                .type(type)
+                .rider(rider)
+                .riderLocation(startPoint)
+                .destination(destination)
+                .distance(distance)
+                .build();
+    }
+}
