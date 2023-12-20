10876cc6570e3d5b24f6fef5a80d236cd91318c2
Samir1498 <samirsbx2@gmail.com>
2023-12-20 07:34:03 +0100

    implementing Ride mapper for the book ride command

---
+++ src/main/java/com/samir/uberweal/application/dtos/RideDto.java
@@ -0,0 +1,18 @@
+package com.samir.uberweal.application.dtos;
+
+import com.samir.uberweal.core.domain.entities.ride.RideStatus;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RideDto {
+    private Long id;
+    private String customerName;
+    private String driverName;
+    private String destination;
+    private String startingPoint;
+    private double price;
+    private double distance;
+    private RideStatus status;
+}
---
+++ src/main/java/com/samir/uberweal/application/queryhandler/RideMapper.java
@@ -0,0 +1,20 @@
+package com.samir.uberweal.application.queryhandler;
+
+import com.samir.uberweal.application.dtos.RideDto;
+import com.samir.uberweal.core.domain.entities.ride.Ride;
+import com.samir.uberweal.core.domain.entities.ride.RideStatus;
+
+public class RideMapper {
+
+    public static RideDto mapToRideDto(Ride ride) {
+        return RideDto.builder()
+                .id(ride.getId())
+                .customerName(ride.getRider().getName())
+                .destination(ride.getDestination().getName())
+                .startingPoint(ride.getStartingPoint().getName())
+                .price(ride.getPrice())
+                .distance(ride.getDistance())
+                .status(RideStatus.COMPLETED)
+                .build();
+    }
+}
