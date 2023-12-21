f521ee71a1712660cc7d48f547e987c30d1d9fed
Samir1498 <samirsbx2@gmail.com>
2023-12-21 15:02:33 +0100

    removing optional error management (handled in the gateway)

--- src/main/java/com/samir/uberweal/application/queryhandler/PreAuthorizeQueryHandler.java
+++ src/main/java/com/samir/uberweal/application/queryhandler/PreAuthorizeQueryHandler.java
@@ -3,28 +3,21 @@ package com.samir.uberweal.application.queryhandler;
 import com.samir.uberweal.application.queries.PreAuthorizeQuery;
 import com.samir.uberweal.domain.entities.Rider;
 import com.samir.uberweal.domain.exceptions.InsufficientFundsException;
-import com.samir.uberweal.domain.exceptions.RiderNotFoundException;
-import com.samir.uberweal.domain.repositories.RiderRepository;
+import com.samir.uberweal.domain.gateways.RiderDsGateway;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 public class PreAuthorizeQueryHandler {
-    private final RiderRepository repository;
+    private final RiderDsGateway repository;
 
     public Rider handle(PreAuthorizeQuery query) {
-        Optional<Rider> riderOptional = repository.findRiderById(query.rider().getId());
+        Rider rider = repository.findRiderById(query.rider().getId());
 
-        if (riderOptional.isPresent()) {
-            double updatedBalance = riderOptional.get().getFunds() - query.amount();
-            if (updatedBalance >= 0) {
-                return riderOptional.get();
-            } else {
-                throw new InsufficientFundsException("Insufficient funds to deduct: " + query.amount());
-            }
+        double updatedBalance = rider.getFunds() - query.amount();
+        if (updatedBalance >= 0) {
+            return rider;
         } else {
-            throw new RiderNotFoundException("Rider not found with ID: " + query.rider().getId());
+            throw new InsufficientFundsException("Insufficient funds to deduct: " + query.amount());
         }
     }
 }
