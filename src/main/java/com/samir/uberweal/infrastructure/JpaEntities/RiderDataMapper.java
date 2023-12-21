54105acab69dd77f41fa3891d6973044a40267dc
Samir1498 <samirsbx2@gmail.com>
2023-12-21 14:58:06 +0100

    setting up spring data jpa

---
+++ src/main/java/com/samir/uberweal/infrastructure/JpaEntities/RiderDataMapper.java
@@ -0,0 +1,25 @@
+package com.samir.uberweal.infrastructure.JpaEntities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="rider")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RiderDataMapper {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private double funds;
+    private LocalDate joinedAt;
+    private boolean voucher;
+}
