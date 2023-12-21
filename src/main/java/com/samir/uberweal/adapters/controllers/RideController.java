0ba40485e44d14840be7dbd8ca99d71887cf8041
Samir1498 <samirsbx2@gmail.com>
2023-12-21 15:00:02 +0100

    creating a rest controller

---
+++ src/main/java/com/samir/uberweal/adapters/controllers/RideController.java
@@ -0,0 +1,50 @@
+package com.samir.uberweal.adapters.controllers;
+
+import com.samir.uberweal.adapters.presenters.BookRideRequest;
+import com.samir.uberweal.adapters.presenters.ListPastRidesRequest;
+import com.samir.uberweal.adapters.dtos.RideDto;
+import com.samir.uberweal.application.commandhandlers.BookRideCommandHandler;
+import com.samir.uberweal.application.commands.BookRideCommand;
+import com.samir.uberweal.application.queries.GetAllRidesQuery;
+import com.samir.uberweal.application.queries.GetRiderQuery;
+import com.samir.uberweal.application.queryhandler.GetAllRidesQueryHandler;
+import com.samir.uberweal.application.queryhandler.GetRiderQueryHandler;
+import com.samir.uberweal.domain.entities.Location;
+import com.samir.uberweal.domain.entities.ride.RideType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/ride")
+@RequiredArgsConstructor
+public class RideController {
+
+    private final GetAllRidesQueryHandler handler;
+    private final GetRiderQueryHandler riderQueryHandler;
+    private final BookRideCommandHandler bookRideCommandHandler;
+
+    @GetMapping
+    public List<RideDto> listPastRides(@RequestBody ListPastRidesRequest request){
+        // mapping request to query 
+        GetAllRidesQuery query = new GetAllRidesQuery(request.riderId());
+        return handler.handle(query);
+    }
+
+    @PostMapping
+    public RideDto bookRide(@RequestBody BookRideRequest request){
+        GetRiderQuery query = new GetRiderQuery(request.riderId());
+        BookRideCommand command = BookRideCommand.builder()
+                .rider(riderQueryHandler.handle(query))
+                //TODO distance calculation
+                .distance(5)
+                .startLocation(new Location(request.startLocation()))
+                .endLocation(new Location(request.endLocation()))
+                .type(RideType.valueOf(request.type()))
+                .build();
+
+        return bookRideCommandHandler.handle(command);
+    }
+
+}
