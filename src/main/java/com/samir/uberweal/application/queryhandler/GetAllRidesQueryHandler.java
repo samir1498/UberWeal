5069a6a010894555a3c8c2bb9cb0d3082bf7ec5f
Samir1498 <samirsbx2@gmail.com>
2023-12-21 15:08:06 +0100

    using gateways instead of a repo

--- src/main/java/com/samir/uberweal/application/queryhandler/GetAllRidesQueryHandler.java
+++ src/main/java/com/samir/uberweal/application/queryhandler/GetAllRidesQueryHandler.java
@@ -1,9 +1,10 @@
 package com.samir.uberweal.application.queryhandler;
 
-import com.samir.uberweal.application.dtos.RideDto;
+import com.samir.uberweal.adapters.dtos.ReMappers;
+import com.samir.uberweal.adapters.dtos.RideDto;
 import com.samir.uberweal.application.queries.GetAllRidesQuery;
 import com.samir.uberweal.domain.entities.ride.Ride;
-import com.samir.uberweal.domain.repositories.RideRepository;
+import com.samir.uberweal.domain.gateways.ListPastRidesDsGateway;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -12,17 +13,16 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class GetAllRidesQueryHandler {
 
-    private final RideRepository rideRepository;
-
+    private final ListPastRidesDsGateway listPastRidesDsGateway;
 
     public List<RideDto> handle(GetAllRidesQuery getAllRidesQuery) {
-        List<Ride> rides = rideRepository.findByRiderId(getAllRidesQuery.riderId());
+        List<Ride> rides = listPastRidesDsGateway.findByRiderId(getAllRidesQuery.riderId());
         return mapToRideDtoList(rides);
     }
 
     private List<RideDto> mapToRideDtoList(List<Ride> rides) {
         return rides.stream()
-                .map(RideMapper::mapToRideDto)
+                .map(ReMappers::RideToRideDto)
                 .collect(Collectors.toList());
     }
 }
