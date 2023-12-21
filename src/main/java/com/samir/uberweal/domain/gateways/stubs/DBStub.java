7ed75ed18d84cd3a271751584e516f72f048356f
Samir1498 <samirsbx2@gmail.com>
2023-12-21 14:58:22 +0100

    creating a DB stub for testing

---
+++ src/main/java/com/samir/uberweal/domain/gateways/stubs/DBStub.java
@@ -0,0 +1,11 @@
+package com.samir.uberweal.domain.gateways.stubs;
+
+import com.samir.uberweal.domain.entities.ride.Ride;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DBStub {
+    public static final Map<Long, Ride> ridesMap = new HashMap<>();
+
+}
