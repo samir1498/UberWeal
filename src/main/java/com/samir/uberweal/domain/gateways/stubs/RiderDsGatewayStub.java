66f09031ec1c1967415e2a0eb74a2bd2930c27e4
Samir1498 <samirsbx2@gmail.com>
2023-12-23 10:17:16 +0100

    throwing if rider is null in findRiderById method in RiderDsGatewayStub.java

--- src/main/java/com/samir/uberweal/domain/gateways/stubs/RiderDsGatewayStub.java
+++ src/main/java/com/samir/uberweal/domain/gateways/stubs/RiderDsGatewayStub.java
@@ -1,6 +1,7 @@
 package com.samir.uberweal.domain.gateways.stubs;
 
 import com.samir.uberweal.domain.entities.Rider;
+import com.samir.uberweal.domain.exceptions.RiderNotFoundException;
 import com.samir.uberweal.domain.gateways.RiderDsGateway;
 
 import java.util.HashMap;
@@ -8,7 +9,7 @@ import java.util.Map;
 
 public class RiderDsGatewayStub implements RiderDsGateway {
 
-    private final Map<Long, Rider> riderDatabase = new HashMap<>();
+    private static final Map<Long, Rider> riderDatabase = new HashMap<>();
     private long currentId = 1;
 
     @Override
@@ -26,6 +27,10 @@ public class RiderDsGatewayStub implements RiderDsGateway {
     @Override
     public Rider findRiderById(Long id) {
         // Retrieve a Rider from the in-memory database by ID
-        return riderDatabase.get(id);
+        Rider rider = riderDatabase.get(id);
+        if(rider == null){
+            throw  new RiderNotFoundException("Rider not found with ID: " + id);
+        }
+        return rider;
     }
 }
