ade9c974e0acf0e51ceb0c2a5104566a2b4350a3
Samir1498 <samirsbx2@gmail.com>
2023-12-24 17:27:39 +0100

    moving spring beans configuration to infra layer

--- src/main/java/com/samir/uberweal/infrastructure/config/AppConfig.java
+++ src/main/java/com/samir/uberweal/infrastructure/config/AppConfig.java
@@ -1,12 +1,17 @@
-package com.samir.uberweal.config;
-import com.samir.uberweal.adapters.gateways.BookRideDsGatewayImpl;
-import com.samir.uberweal.adapters.gateways.ListPastRidesDsGatewayImpl;
-import com.samir.uberweal.adapters.gateways.RiderDsGatewayImpl;
+package com.samir.uberweal.infrastructure.config;
+import com.samir.uberweal.infrastructure.gateways.BookRideDsGatewayImpl;
+import com.samir.uberweal.infrastructure.gateways.ListPastRidesDsGatewayImpl;
+import com.samir.uberweal.infrastructure.gateways.RiderDsGatewayImpl;
 import com.samir.uberweal.application.query.handler.GetAllRidesQueryHandler;
 import com.samir.uberweal.application.query.handler.GetRiderQueryHandler;
-import com.samir.uberweal.domain.gateways.BookRideDsGateway;
-import com.samir.uberweal.domain.gateways.ListPastRidesDsGateway;
-import com.samir.uberweal.domain.gateways.RiderDsGateway;
+import com.samir.uberweal.adapters.gateways.BookRideDsGateway;
+import com.samir.uberweal.adapters.gateways.ListPastRidesDsGateway;
+import com.samir.uberweal.adapters.gateways.RiderDsGateway;
+import com.samir.uberweal.domain.services.discount.DiscountService;
+import com.samir.uberweal.domain.services.pricing.RideChargeCalculatorFactory;
+import com.samir.uberweal.domain.services.pricing.FinalChargeCalculator;
+import com.samir.uberweal.domain.services.pricing.strategies.JourneyChargeCalculator;
+import com.samir.uberweal.domain.services.pricing.strategies.TripChargeCalculator;
 import com.samir.uberweal.infrastructure.jpa.repositories.JpaRideRepository;
 import com.samir.uberweal.infrastructure.jpa.repositories.JpaRiderRepository;
 import lombok.RequiredArgsConstructor;
@@ -44,5 +49,20 @@ public class AppConfig {
     public GetRiderQueryHandler getRiderQueryHandler(){
         return new GetRiderQueryHandler(riderDsGateway());
     }
+
+
+    @Bean
+    public RideChargeCalculatorFactory calculatorFactory(){
+        return new RideChargeCalculatorFactory(new TripChargeCalculator(), new JourneyChargeCalculator());
+    }
+    @Bean
+    public DiscountService discountService(){
+        return new DiscountService();
+    }
+    @Bean
+    public FinalChargeCalculator priceCalculator(){
+        return new FinalChargeCalculator(discountService(), calculatorFactory());
+    }
+
 
 }
